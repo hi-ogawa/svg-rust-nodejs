9fb0659e875e086f606e865654184aff798affd5
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2022-06-27 00:45:34 +0900

    feat: add "GET /svg2png" endpoint

--- app/src/app.ts
+++ app/src/app.ts
@@ -1,6 +1,7 @@
-import fastify from "fastify";
-import { getTemporaryFile, sortKeys } from "./utils";
 import fs from "fs";
+import { IncomingMessage } from "http";
+import fastify, { FastifyReply } from "fastify";
+import { getTemporaryFile, sortKeys } from "./utils";
 import { svgToPng } from "@hiogawa/svg-rust-nodejs";
 
 export const app = fastify({
@@ -13,10 +14,22 @@ app.addContentTypeParser("*", function (_request, _payload, done) {
 });
 
 app.get("/", (req) => {
-  const url = `https://${req.hostname}/svg2png`;
+  const protocol = process.env.NODE_ENV === "production" ? "https" : "http";
+  const url = `${protocol}://${req.hostname}/svg2png`;
   const svg =
     '<svg xmlns="http://www.w3.org/2000/svg" width="300" height="200" viewBox="0 0 900 600"><rect fill="#fff" height="600" width="900"/><circle fill="#bc002d" cx="450" cy="300" r="180"/></svg>';
-  const example = `example:\n\n    curl ${url} -d '${svg}' > test.png\n`;
+  const example = `
+## example
+
+- GET
+
+${url}?svg=${encodeURIComponent(svg)}
+
+- POST
+
+curl ${url} -d '${svg}' > test.png
+
+`;
   return example;
 });
 
@@ -26,11 +39,27 @@ app.get("/debug", async () => {
   };
 });
 
+app.get("/svg2png", async (req, res) => {
+  const { svg } = req.query as any;
+  if (typeof svg !== "string") {
+    res.code(400).send({ message: "[ERROR] invalid parameter: svg" });
+    return;
+  }
+  await svg2pngHandler(svg, res);
+});
+
 app.post("/svg2png", async (req, res) => {
+  await svg2pngHandler(req.raw, res);
+});
+
+async function svg2pngHandler(
+  input: string | IncomingMessage,
+  res: FastifyReply
+) {
   const inFile = getTemporaryFile();
   const outFile = getTemporaryFile();
   try {
-    await fs.promises.writeFile(inFile, req.raw);
+    await fs.promises.writeFile(inFile, input);
 
     // TODO: use worker_threads
     svgToPng(inFile, outFile);
@@ -43,4 +72,4 @@ app.post("/svg2png", async (req, res) => {
     fs.rmSync(inFile);
     fs.rmSync(outFile);
   }
-});
+}
