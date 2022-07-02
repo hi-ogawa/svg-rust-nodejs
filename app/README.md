630da4b91cf6883e1b2ce3747c80cb75a8c9082a
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2022-07-02 19:26:07 +0900

    feat: hack dominant-baseline="central"

--- app/README.md
+++ app/README.md
@@ -9,13 +9,11 @@ npm run dev
 
 # example
 curl http://127.0.0.1:3000/svg2png -d '<svg xmlns="http://www.w3.org/2000/svg" width="300" height="200" viewBox="0 0 900 600"><rect fill="#fff" height="600" width="900"/><circle fill="#bc002d" cx="450" cy="300" r="180"/></svg>' > test.png
+curl http://127.0.0.1:3000/svg2png -d @./misc/examples/test.svg > ./misc/examples/test.png
 
 # deployment
 vercel projects add svg2png-hiro18181
 vercel link -p svg2png-hiro18181
 npm run deploy
 npm run deploy:production
-
-# example
-curl https://svg2png-hiro18181.vercel.app/svg2png -d '<svg xmlns="http://www.w3.org/2000/svg" width="300" height="200" viewBox="0 0 900 600"><rect fill="#fff" height="600" width="900"/><circle fill="#bc002d" cx="450" cy="300" r="180"/></svg>' > test.vercel.png
 ```
---
+++ app/src/hack-dominant-baseline.ts
@@ -0,0 +1,28 @@
+import { streamToString } from "./utils";
+
+export function hackDominantBaseline(data: string): string {
+  return data.replaceAll(/<text(.*?)>/g, replaceText);
+}
+
+// support only echarts/zrender's svg output https://github.com/ecomfe/zrender/blob/4b664fbdc32060032a35d1d39a4129264dbb44df/src/svg/graphic.ts#L276-L306
+const FONT_SIZE_RE = /font-size:(\d+)px/;
+const DOMINANT_BASELINE_RE = /dominant-baseline="(\w+)"/;
+
+function replaceText(textTag: string): string {
+  const fontSize = textTag.match(FONT_SIZE_RE);
+  const dominantBaseline = textTag.match(DOMINANT_BASELINE_RE);
+  if (fontSize && dominantBaseline && dominantBaseline[1] === "central") {
+    const dy = Number(fontSize[1]) / 3;
+    return textTag.replace(DOMINANT_BASELINE_RE, `dy="${dy}"`);
+  }
+  return textTag;
+}
+
+async function main() {
+  const stdin = await streamToString(process.stdin);
+  process.stdout.write(hackDominantBaseline(stdin));
+}
+
+if (process.env.NODE_ENV !== "production" && require.main === module) {
+  main();
+}
