a1850128c877015aed31fe53c2ba1bbe7919e156
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2022-07-03 10:34:51 +0900

    feat: no panic on error (#4)

--- src/svg_to_png.rs
+++ src/svg_to_png.rs
@@ -1,3 +1,4 @@
+use napi::bindgen_prelude::{Error, Result, Status};
 use napi_derive;
 use resvg;
 use tiny_skia;
@@ -9,7 +10,7 @@ pub fn svg_to_png(
     out_file: String,
     font_family: Option<String>,
     font_file: Option<String>,
-) {
+) -> Result<()> {
     // configuration
     let mut opts = usvg::Options::default();
     opts.font_size = 16.0; // TODO: configurable
@@ -18,26 +19,29 @@ pub fn svg_to_png(
     {
         opts.font_family = "Roboto".to_string();
         let font_data = std::include_bytes!("../misc/fonts/roboto-android/Roboto-Regular.ttf");
-        opts.fontdb.load_font_data(font_data.to_vec());
+        opts.fontdb.load_font_data(font_data.to_vec()); // logging only when error
     }
 
     if let Some(font_family) = font_family {
         opts.font_family = font_family;
     }
     if let Some(font_file) = font_file {
-        opts.fontdb.load_font_file(font_file).unwrap();
+        opts.fontdb.load_font_file(font_file)?;
     }
 
     // conver svg to usvg tree
-    let svg = std::fs::read(in_file).unwrap();
-    let tree = usvg::Tree::from_data(&svg, &opts.to_ref()).unwrap();
+    let svg = std::fs::read(in_file)?;
+    let tree = usvg::Tree::from_data(&svg, &opts.to_ref()).map_err(to_generic_failure)?;
 
     // render to pixmap
     let screen_size = tree.svg_node().size.to_screen_size();
-    let mut pixmap = tiny_skia::Pixmap::new(screen_size.width(), screen_size.height()).unwrap();
+    let mut pixmap = tiny_skia::Pixmap::new(screen_size.width(), screen_size.height())
+        .ok_or(to_generic_failure("tiny_skia::Pixmap::new error"))?;
 
     // while background (TODO: configurable)
-    pixmap.fill(tiny_skia::Color::from_rgba(1.0, 1.0, 1.0, 1.0).unwrap());
+    let background = tiny_skia::Color::from_rgba(1.0, 1.0, 1.0, 1.0)
+        .ok_or(to_generic_failure("tiny_skia::Color::from_rgba error"))?;
+    pixmap.fill(background);
 
     resvg::render(
         &tree,
@@ -45,8 +49,13 @@ pub fn svg_to_png(
         tiny_skia::Transform::default(),
         pixmap.as_mut(),
     )
-    .unwrap();
+    .ok_or(to_generic_failure("resvg::render error"))?;
 
     // write png
-    pixmap.save_png(out_file).unwrap();
+    pixmap.save_png(out_file).map_err(to_generic_failure)?;
+    Ok(())
+}
+
+fn to_generic_failure<T: ToString>(e: T) -> Error {
+    Error::new(Status::GenericFailure, e.to_string())
 }
