4901999689624df59bb2a20f56068b1dd1b472d8
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2022-07-03 16:35:35 +0900

    feat: add `fontUrl` parameter to load custom font

--- app/src/app.ts
+++ app/src/app.ts
@@ -5,6 +5,7 @@ import { svgToPng } from "@hiogawa/svg-rust-nodejs";
 import { fetch } from "undici";
 import { hackDominantBaseline } from "./hack-dominant-baseline";
 import { resolveExternalHref } from "./resolve-external-href";
+import { z } from "zod";
 
 export const app = fastify({
   logger: true,
@@ -15,20 +16,36 @@ app.addContentTypeParser("*", function (_request, _payload, done) {
   done(null);
 });
 
+//
+// GET /
+//
+
 app.get("/", (req) => {
   const protocol = process.env.NODE_ENV === "production" ? "https" : "http";
   const url = `${protocol}://${req.hostname}/svg2png`;
   const svg =
     '<svg xmlns="http://www.w3.org/2000/svg" width="300" height="200" viewBox="0 0 900 600"><rect fill="#fff" height="600" width="900"/><circle fill="#bc002d" cx="450" cy="300" r="180"/></svg>';
+  const svgText =
+    '<svg xmlns="http://www.w3.org/2000/svg" width="300" height="200"><text x="50%" y="50%" text-anchor="middle" dominant-baseline="central" style="font-size:40px;" fill="#000">日本語テスト</text></svg>';
   const example = `
 
 # example
 
 - GET
 
+  - url parameter
+
 ${url}?url=https://raw.githubusercontent.com/hi-ogawa/svg-rust-nodejs/master/misc/examples/test.svg
 
+  - svg parameter
+
 ${url}?svg=${encodeURIComponent(svg)}
+
+  - fontUrl parameter
+
+${url}?svg=${encodeURIComponent(
+    svgText
+  )}&fontUrl=https://rawcdn.githack.com/googlefonts/noto-cjk/acb30accc6b2aa8838c6cfbf0486de28358a657b/Sans/OTF/Japanese/NotoSansCJKjp-Regular.otf
 
 - POST
 
@@ -40,42 +57,89 @@ curl ${url} -d '${svg}' > test.png
   return example;
 });
 
+//
+// GET /debug
+//
+
 app.get("/debug", async () => {
   return {
     "process.env": sortKeys(process.env),
   };
 });
 
-app.get("/svg2png", async (req, res) => {
-  const { svg, url } = req.query as any;
-  if (typeof url === "string") {
-    const urlRes = await fetch(url);
-    const urlSvg = await urlRes.text();
-    await svg2pngHandler(urlSvg, res);
-    return;
-  }
-  if (typeof svg === "string") {
-    await svg2pngHandler(svg, res);
-    return;
-  }
-  res.code(400).send({ message: "[ERROR] invalid parameters" });
+//
+// GET /svg2png
+//
+
+const GET_SCHEMA = z.object({
+  svg: z.string().optional(),
+  url: z.string().optional(),
+  fontFamily: z.string().optional(),
+  fontUrl: z.string().url().optional(),
 });
+
+app.get("/svg2png", async (req, res) => {
+  const parsed = GET_SCHEMA.safeParse(req.query);
+  if (!parsed.success) {
+    res.code(400).send({ message: "invalid parameters" });
+    return;
+  }
+  let svg: string;
+  if (parsed.data.url) {
+    const urlRes = await fetch(parsed.data.url);
+    svg = await urlRes.text();
+  } else if (parsed.data.svg) {
+    svg = parsed.data.svg;
+  } else {
+    res.code(400).send({ message: "invalid parameters" });
+    return;
+  }
+  await svg2pngHandler(svg, res, parsed.data.fontFamily, parsed.data.fontUrl);
+});
+
+//
+// POST / svg2png
+//
+
+const POST_SCHEMA = GET_SCHEMA.pick({ fontFamily: true, fontUrl: true });
 
 app.post("/svg2png", async (req, res) => {
+  const parsed = POST_SCHEMA.safeParse(req.query);
+  if (!parsed.success) {
+    res.code(400).send({ message: "invalid parameters" });
+    return;
+  }
   const svg = await streamToString(req.raw);
-  await svg2pngHandler(svg, res);
+  await svg2pngHandler(svg, res, parsed.data.fontFamily, parsed.data.fontUrl);
 });
 
-async function svg2pngHandler(svg: string, res: FastifyReply) {
+//
+// utils
+//
+
+async function svg2pngHandler(
+  svg: string,
+  res: FastifyReply,
+  fontFamily?: string,
+  fontUrl?: string
+) {
   const inFile = getTemporaryFile();
   const outFile = getTemporaryFile();
+  const fontFile = getTemporaryFile();
   try {
     svg = hackDominantBaseline(svg);
     svg = await resolveExternalHref(svg);
     await fs.promises.writeFile(inFile, svg);
 
+    if (fontUrl) {
+      const fontRes = await fetch(fontUrl);
+      if (fontRes.body) {
+        await fs.promises.writeFile(fontFile, fontRes.body);
+      }
+    }
+
     // TODO: use worker_threads
-    svgToPng(inFile, outFile);
+    svgToPng(inFile, outFile, fontFamily, fontUrl ? fontFile : undefined);
 
     const outData = await fs.promises.readFile(outFile);
 
@@ -84,5 +148,6 @@ async function svg2pngHandler(svg: string, res: FastifyReply) {
   } finally {
     fs.rmSync(inFile, { force: true });
     fs.rmSync(outFile, { force: true });
+    fs.rmSync(fontFile, { force: true });
   }
 }
